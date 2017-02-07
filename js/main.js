703a95f18b9dbf8f52d2fec1798cba0ff643ec58
Gio <orurog@gmail.com>
2017-02-07 02:26:29 -0500

    moficacion funcion para eliminartareas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,71 @@
-var lista=document.getElementById("lista");
+var boton=document.getElementById("boton");
+boton.setAttribute("onclick","onButtonClick()");
+
+var tareas=[];
+// texto
+// check
+
+function onButtonClick(){
+    var textArea=document.getElementById("texto");
+    tareas.push({texto:textArea.value,check:false});
+    textArea.value="";
+    textArea.focus();
+        crearElementos();
+}
+
+function crearElementos(){
+    var lista= document.getElementById("lista");
+    lista.innerHTML="";
+    for (var i in tareas){
+        var html= "<li class='tarea'><input onclick='onChecked("+i+")' class='check' type='checkbox' "+(tareas[i].check?"checked":"")+">"+"<span "+(tareas[i].check?"style='text-decoration:line-through'":"style='text-decoration:none'")+" id='new'>"+tareas[i].texto+"</span><i onclick='eliminarSpan("+i+")' class='glyphicon glyphicon-trash tacho'></i></li>";
+        
+        lista.innerHTML+=html;
+    }
+}
+
+function onChecked(pos){
+    tareas[pos].check = !tareas[pos].check;
+    crearElementos();
+   /* if( tareas[pos].check==false){
+        tareas[pos].check=true;
+    }else{
+        tareas[pos].check=false;
+    }
+    crearElementos();*/
+}
+function eliminarSpan(trash){
+    tareas.splice(trash,1)
+    crearElementos();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var lista=document.getElementById("lista");
 
 function anadirTarea(){
 	
@@ -48,4 +115,4 @@ function anadirTarea(){
 	{
 		check.nextSibling.setAttribute("style","text-decoration: none");
 	}	
-}
+}*/
